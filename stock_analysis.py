dc50bd47dbecffde4a1f6f1c87c01a02197ecbf7
Mark Lau <91698553+mlau25@users.noreply.github.com>
2023-11-25 17:23:00 -0500

    Update stock_analysis.py
    updates for stock_analysis.py for evadb2

--- stock_analysis.py
+++ stock_analysis.py
@@ -2,11 +2,12 @@ import os
 import pandas as pd
 import evadb
 import psycopg2
+import openai
 
 
 db_params = {
     'user': 'postgres',
-    'password': 'bbqporkbun25$',
+    'password': 'xxclassifiedxx',
     'host': 'localhost',
     'port': '5432',
     'database': 'postgres'
@@ -154,6 +155,107 @@ def setup_ai_funcs():
 
     cursor.close()
 
+def generate_analysis_summary(ticker: str):
+    cursor = evadb.connect().cursor()
+
+    df = cursor.query(f"USE postgres_stock_data {{SELECT * FROM stock_data WHERE stock_symbol = '{ticker}' ORDER BY date DESC LIMIT 10}}").df()
+
+    selected_columns = ['stock_symbol', 'date', 'open', 'close', 'high', 'low', 'volume']
+    filtered_data = df[selected_columns].copy()
+
+    # Converting data to a dictionary
+    data = filtered_data.to_dict(orient='list')
+
+    stock_data = pd.DataFrame(data)
+
+    # Generating a textual summary of the stock data
+    summary = f"Summary of {ticker} stock data:\n"
+    summary += f"Date Range: {stock_data['date'].min()} to {stock_data['date'].max()}\n"
+    summary += f"Opening Price Range: ${stock_data['open'].min()} to ${stock_data['open'].max()}\n"
+    summary += f"Closing Price Range: ${stock_data['close'].min()} to ${stock_data['close'].max()}\n"
+    summary += f"Highest Price Recorded: ${stock_data['high'].max()}\n"
+    summary += f"Lowest Price Recorded: ${stock_data['low'].min()}\n"
+    summary += f"Total Volume Traded: {stock_data['volume'].sum()} shares\n"
+
+    return summary
+
+def create_digest():
+    client = openai.OpenAI(
+    api_key='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',
+    )
+
+    messages = [ {"role": "system", "content":  
+              "You are an intelligent assistant."} ]
+
+    for ticker in stocks:
+        summary = generate_analysis_summary(ticker)
+        messages.append( 
+            {"role": "user", "content": summary}, 
+        )
+
+    messages.append(
+        {"role": "user", "content": "Create a news digest of all the provided stocks, and provide only a high level overview"}
+    )
+    chat = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages 
+    )
+    reply = chat.choices[0].message.content 
+
+    messages.append({"role": "assistant", "content": reply})
+
+    return reply
+
+def create_analysis_report(ticker: str):
+    client = openai.OpenAI(
+    api_key='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',
+    )
+
+    messages = [ {"role": "system", "content":  
+              "You are an intelligent assistant."} ]
+
+    summary = generate_analysis_summary(ticker)
+
+    messages.append( 
+        {"role": "user", "content": summary}, 
+    )
+    messages.append(
+        {"role": "user", "content": "Generate an analysis report on the stock prices provided for the stock"}
+    )
+    chat = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages 
+    )
+    reply = chat.choices[0].message.content 
+
+    messages.append({"role": "assistant", "content": reply})
+
+    return reply
+
+def create_comparison_report(ticker1: str, ticker2: str):
+    client = openai.OpenAI(
+    api_key='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',
+    )
+
+    messages = [ {"role": "system", "content":  
+              "You are an intelligent assistant."} ]
+
+    for ticker in [ticker1, ticker2]:
+        summary = generate_analysis_summary(ticker)
+        messages.append( 
+            {"role": "user", "content": summary}, 
+        )
+    
+    messages.append(
+        {"role": "user", "content": "Create a comparison report between the two stocks and their provided stock data"}
+    )
+    chat = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages 
+    )
+    reply = chat.choices[0].message.content 
+    # print(f"ChatGPT: {reply}") 
+    messages.append({"role": "assistant", "content": reply})
+
+    return reply
+
 # merges multiple stock .csv files into one
 def merge_stock_data():
     merged_data = pd.DataFrame()
@@ -171,7 +273,7 @@ def merge_stock_data():
     
     return merged_data
 
-# upload merged data to postgres
+# upload merged data to evadb
 def upload_stock_data(merged_data: pd.DataFrame):
     # SQL command to insert fresh data
     cursor = evadb.connect().cursor()
@@ -182,13 +284,41 @@ def upload_stock_data(merged_data: pd.DataFrame):
     # connection.commit()
 
     # iteratively insert data row by row
-    for idx, row in merged_data.iterrows():
-        insert_row = f"""
+    # for idx, row in merged_data.iterrows():
+    #     insert_row = f"""
+    #     USE postgres_stock_data {{
+    #     INSERT INTO stock_data VALUES ('{row['stock_symbol']}', '{row['date']}', '{row['open']}', '{row['close']}', '{row['high']}', '{row['low']}', '{row['volume']}')
+    #     }}
+    #     """
+    #     cursor.query(insert_row).df()
+
+    # cursor.close()
+
+    # Updated insert method
+    if 'adj close' in merged_data.columns:
+        merged_data.drop(columns='adj close', inplace=True)
+
+    rows = [tuple(row) for row in merged_data.values]
+
+    batch_size = 5000
+    # Batch insert data into the database
+    for i in range(0, len(rows), batch_size):
+        # Get a batch of rows
+        batch = rows[i:i + batch_size]
+
+        # Create the string representation of the batch
+        insert_values = ", ".join(["('" + "', '".join(map(str, row)) + "')" for row in batch])
+
+        # Create the full SQL INSERT query for this batch
+        insert_query = f"""
         USE postgres_stock_data {{
-        INSERT INTO stock_data VALUES ('{row['stock_symbol']}', '{row['date']}', '{row['open']}', '{row['close']}', '{row['high']}', '{row['low']}', '{row['volume']}')
+            INSERT INTO stock_data (date, open, high, low, close, volume, stock_symbol)
+            VALUES {insert_values}
         }}
         """
-        cursor.query(insert_row).df()
+
+        # Execute the INSERT query
+        cursor.query(insert_query).df()
 
     cursor.close()
 
